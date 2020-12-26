c6eb342484dededc4ea404a9cd1e577f634322b9
Rune Nielsen <runenielsen@runbox.com>
2020-12-26 20:30:08 +0100

    adds section about conditionals

--- src/examples.lisp
+++ src/examples.lisp
@@ -14,3 +14,49 @@
   (if list
       (1+ (my-length (cdr list)))
       0))
+
+;;; Page 51 - Any value that is not equivalent to an empty list will
+;;; be considered true
+(eq '() nil)   ; ==> T
+(eq '() ())    ; ==> T
+(eq '() 'nil)  ; ==> T
+
+;;; Page 52 - conditionals
+(if (= (+ 1 2) 3)
+    'yup
+    'nop) ; ==> YUP
+
+(if (= (+ 1 2) 4)
+    'yup
+    'nop) ; ==> NOP
+
+(if '(1)
+    'the-list-has-stuff-in-it
+    'the-list-is-empty) ; ==> THE-LIST-HAS-STUFF-IN-IT
+
+(if '()
+    'the-list-has-stuff-in-it
+    'the-list-is-empty) ; ==> THE-LIST-IS-EMPTY
+
+(if (oddp 5)
+    'odd-number
+    'even-number) ; ==> ODD-NUMBER
+
+;;; Page 54 - To do two or more things in an 'if' branch use 'progn'.
+(defvar *number-was-odd* nil)
+
+(if (oddp 5)
+    (progn (setf *number-was-odd* t)
+           'odd-number)
+    'even-number) ; => *number-was-odd* will eval to T
+
+;;; Page 55 - 'when' and 'unless' has implicit 'progn'
+(defvar *number-is-odd* nil)
+
+(when (oddp 5)
+  (setf *number-is-odd* t)
+  'odd-number) ; => *number-is-odd* will eval to T and returns 'odd-number
+
+(unless (oddp 4)
+  (setf *number-is-odd* nil)
+  'even-number) ; => *number-is-odd* will eval to NIL and returns 'even-number
